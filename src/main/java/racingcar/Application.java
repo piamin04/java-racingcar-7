fdc094b15f2780fd8e01a8a91b934d08e46de7ab
piamin04 <ljmin@soongsil.ac.kr>
2025-02-14 16:35:54 +0900

    docs(README): 리드미 작성 구현 기능 목록 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
 public class Application {
+    public class Car{
+        public static String cut(String input){
+            ArrayList<String> carlist = new ArrayList<>();
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = Console.readLine();
+
+
     }
 }
