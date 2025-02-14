64b9a40f0e9d699326e04d32061c7c192257ac7d
piamin04 <ljmin@soongsil.ac.kr>
2025-02-14 17:00:53 +0900

    feat(feature) : 함수 작성 자동차 이름 검수 및 쉼표를 기준으로 나누는 함수 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,16 +4,41 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
-    public class Car{
-        public static String cut(String input){
-            ArrayList<String> carlist = new ArrayList<>();
+    public static class Car{
+        //1. 자동차 이름을 검수하고 쉼표를 기준으로 나누는 함수
+        public static List<String> cut(String input){
+            if (input == null || input.trim().isEmpty()) {
+                throw new IllegalArgumentException("error");
+            }
+
+            List<String> carList = new ArrayList<>(Arrays.asList(input.split(",")));
+
+            for (String carName : carList) {
+                carName = carName.trim(); // 공백 제거
+                if (carName.isEmpty()) {
+                    throw new IllegalArgumentException("error");
+                }
+                if (carName.length() > 5) {
+                    throw new IllegalArgumentException("error");
+                }
+            }
+            return carList;
         }
+
     }
+
     public static void main(String[] args) {
+        System.out.println("Input String: ");
         String input = Console.readLine();
 
+        //자동차 생성 및 이름 리스트로 저장
+        List<String> carNames = Car.cut(input);
 
+        // 테스트 출력
+        System.out.println(carNames);
     }
 }
